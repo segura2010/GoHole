dfae9ca7ff1d2563998bccef04beef7a67b424a5
alberto__segura <alberto.segura.delgado@gmail.com>
2017-08-06 10:29:32 +0200

    improved parseQuery and added clientIp

--- dnsserver/server.go
+++ dnsserver/server.go
@@ -12,88 +12,59 @@ import (
     "GoHole/dnscache"
 )
 
-func parseQuery(m *dns.Msg) {
+func parseQuery(clientIp string, m *dns.Msg) {
 	for _, q := range m.Question {
-		switch q.Qtype {
-		case dns.TypeA:
-			// IPv4
-			log.Printf("Query for %s\n", q.Name)
-			cleanedName := q.Name[0:len(q.Name)-1] // remove the end "."
-			ip, err := dnscache.GetDomainIPv4(cleanedName)
-			log.Printf("IP: %s", ip)
-			if ip != "" && err == nil {
-				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
-				if err == nil {
-					m.Answer = append(m.Answer, rr)
-				}
-			}else{
-				// Request to a DNS server
-				c := new(dns.Client)
-				msg := new(dns.Msg)
-				msg.SetQuestion(dns.Fqdn(q.Name), dns.TypeA)
-				msg.RecursionDesired = true
 
-			    r, _, err := c.Exchange(msg, net.JoinHostPort(config.GetInstance().UpstreamDNSServer, "53"))
-			    if r == nil {
-			    	log.Printf("*** error: %s\n", err.Error())
-			    	return
-			    }
+		log.Printf("Query for %s from %s", q.Name, clientIp)
 
-			    if r.Rcode != dns.RcodeSuccess {
-			    	log.Printf(" *** invalid answer name %s after A query for %s\n", q.Name, q.Name)
-			    	return
-			    }
-			    // Parse Answer
-			    for _, a := range r.Answer {
-			    	ans := strings.Split(a.String(), "\t")
-			    	if len(ans) == 5 && ans[3] == "A"{
-			    		// Save on cache
-			    		dnscache.AddDomainIPv4(cleanedName, ans[4], config.GetInstance().DomainCacheTime)
-			    	}
-			    }
-			    // Set answer for the client
-			    m.Answer = r.Answer
+		var err error = nil
+		var ip string = ""
+		cleanedName := q.Name[0:len(q.Name)-1] // remove the end "."
+		qType := "A"
+
+		if q.Qtype == dns.TypeA{
+			ip, err = dnscache.GetDomainIPv4(cleanedName)
+		}else if q.Qtype == dns.TypeAAAA{
+			ip, err = dnscache.GetDomainIPv6(cleanedName)
+			qType = "AAAA"
+		}
+
+		if ip != "" && err == nil {
+			rr, err := dns.NewRR(fmt.Sprintf("%s %s %s", q.Name, qType, ip))
+			if err == nil {
+				m.Answer = append(m.Answer, rr)
 			}
+		}else{
+			// Request to a DNS server
+			c := new(dns.Client)
+			msg := new(dns.Msg)
+			msg.SetQuestion(dns.Fqdn(q.Name), q.Qtype)
+			msg.RecursionDesired = true
 
-		case dns.TypeAAAA:
-			// IPv6
-			log.Printf("Query for %s\n", q.Name)
-			cleanedName := q.Name[0:len(q.Name)-1] // remove the end "."
-			ip, err := dnscache.GetDomainIPv6(cleanedName)
-			log.Printf("IP: %s", ip)
-			if ip != "" && err == nil {
-				rr, err := dns.NewRR(fmt.Sprintf("%s AAAA %s", q.Name, ip))
-				if err == nil {
-					m.Answer = append(m.Answer, rr)
-				}
-			}else{
-				// Request to a DNS server
-				c := new(dns.Client)
-				msg := new(dns.Msg)
-				msg.SetQuestion(dns.Fqdn(q.Name), dns.TypeAAAA)
-				msg.RecursionDesired = true
+		    r, _, err := c.Exchange(msg, net.JoinHostPort(config.GetInstance().UpstreamDNSServer, "53"))
+		    if r == nil {
+		    	log.Printf("*** error: %s\n", err.Error())
+		    	return
+		    }
 
-			    r, _, err := c.Exchange(msg, net.JoinHostPort(config.GetInstance().UpstreamDNSServer, "53"))
-			    if r == nil {
-			    	log.Printf("*** error: %s\n", err.Error())
-			    	return
-			    }
-
-			    if r.Rcode != dns.RcodeSuccess {
-			    	log.Printf(" *** invalid answer name %s after AAAA query for %s\n", q.Name, q.Name)
-			    	return
-			    }
-			    // Parse Answer
-			    for _, a := range r.Answer {
-			    	ans := strings.Split(a.String(), "\t")
-			    	if len(ans) == 5 && ans[3] == "AAAA"{
-			    		// Save on cache
-			    		dnscache.AddDomainIPv6(cleanedName, ans[4], config.GetInstance().DomainCacheTime)
-			    	}
-			    }
-			    // Set answer for the client
-			    m.Answer = r.Answer
-			}
+		    if r.Rcode != dns.RcodeSuccess {
+		    	log.Printf(" *** invalid answer name %s after %s query for %s\n", q.Name, qType, q.Name)
+		    	return
+		    }
+		    // Parse Answer
+		    for _, a := range r.Answer {
+		    	ans := strings.Split(a.String(), "\t")
+		    	if len(ans) == 5 && ans[3] == qType{
+		    		// Save on cache
+		    		if q.Qtype == dns.TypeA{
+		    			dnscache.AddDomainIPv4(cleanedName, ans[4], config.GetInstance().DomainCacheTime)
+					}else if q.Qtype == dns.TypeAAAA{
+						dnscache.AddDomainIPv6(cleanedName, ans[4], config.GetInstance().DomainCacheTime)
+					}
+		    	}
+		    }
+		    // Set answer for the client
+		    m.Answer = r.Answer
 		}
 	}
 }
@@ -102,10 +73,12 @@ func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Compress = false
+	clientIp := w.RemoteAddr().String()
+	clientIp = clientIp[0:strings.LastIndex(clientIp, ":")] // remove port
 
 	switch r.Opcode {
 	case dns.OpcodeQuery:
-		parseQuery(m)
+		parseQuery(clientIp, m)
 	}
 
 	w.WriteMsg(m)
